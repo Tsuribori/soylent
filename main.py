3e15bd00f30a23be33f607cc3c40efc443127c4f
Tsuribori <tsuribori@tutanota.com>
2018-02-10 14:29:04 +0200

    Freezing and unfreezing of packages, better error handling

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ if os.path.exists(db_name) == False:
     if os.access(os.path.dirname(os.path.realpath(__file__)), os.W_OK):
         conn = sqlite3.connect(db_name)
         c = conn.cursor()
-        c.execute('CREATE TABLE repos (name TEXT PRIMARY KEY, path TEXT NOT NULL)')
+        c.execute('CREATE TABLE repos (name TEXT PRIMARY KEY, path TEXT NOT NULL, frozen INTEGER NOT NULL)')
         conn.commit()
         conn.close()
     else:
@@ -19,13 +19,20 @@ def install(repo):
     if os.access(os.getcwd(), os.W_OK):
         conn = sqlite3.connect(db_name)
         c = conn.cursor()
-        repo_real = repo.split("/")
-        path = os.getcwd() + "/" + repo_real[1]
+        try:
+            repo_real = repo.split("/")
+            path = os.getcwd() + "/" + repo_real[1]
+        except IndexError:
+            sys.exit("Repo name must be in format username/repo")
         try: 
-            c.execute("INSERT INTO repos (name, path) VALUES ('{name}', '{path}')".format(name=repo, path=path))  
-            conn.commit()
-            conn.close()
-            os.system(command)
+            os.system(command) 
+            if os.path.isdir(path):
+                c.execute("INSERT INTO repos (name, path, frozen) VALUES ('{name}', '{path}', 0)".format(name=repo, path=path))  
+                conn.commit()
+                conn.close()
+                sys.exit("Repo installed succesfully.")
+            else:
+                sys.exit("\nCould not install repo")
         except sqlite3.IntegrityError:
              sys.exit("Repo already installed")
     else:
@@ -36,7 +43,10 @@ def uninstall(repo):
         c = conn.cursor()
         c.execute("SELECT path FROM repos WHERE name = '{name}'".format(name=repo))
         repo_path = c.fetchone()
-        command = "rm -R -f " + repo_path[0]
+        try:
+            command = "rm -R -f " + repo_path[0]
+        except TypeError:
+            sys.exit("Repo not installed")
         try:
             c.execute("DELETE FROM repos WHERE name = '{name}'".format(name=repo))
             conn.commit()
@@ -45,18 +55,75 @@ def uninstall(repo):
             sys.exit("Repo uninstalled successfully")
         except sqlite3.IntegrityError:
             sys.exit("Error while deleting repo from database")
-    sys.exit("Error: Cannot write into directory")
+    
+    else:
+        sys.exit("Error: Cannot write into directory where " + repo + " resides.")
+
+def update():
+    conn = sqlite3.connect(db_name)
+    c = conn.cursor()
+    c.execute("SELECT path, name FROM repos WHERE frozen = 0")
+    paths = c.fetchall()
+    for i in paths:
+        if os.access(os.getcwd(), os.W_OK):
+            print("\n" + "Updating " + i[1])
+            os.chdir(i[0])
+            os.system("git pull origin master")
+        else:
+            print("Error: No write access for " + i[1])
+    conn.close()
+
+def freeze(repo):
+    conn = sqlite3.connect(db_name)
+    c = conn.cursor()
+    c.execute("SELECT path, frozen FROM repos WHERE name = '{name}'".format(name=repo))
+    path = c.fetchall()
+    try:
+        if path[0][0] and path[0][1] == 0:
+            if c.execute("UPDATE repos SET frozen = 1 WHERE name = '{name}'".format(name=repo)):
+                conn.commit()
+                conn.close()
+                print(repo + " frozen.")
+        else:
+            sys.exit("Repo already frozen.")
+    except TypeError and IndexError:
+        sys.exit("Repo not installed.")
+ 
+def unfreeze(repo):
+    conn = sqlite3.connect(db_name)
+    c = conn.cursor()
+    c.execute("SELECT path, frozen FROM repos WHERE name = '{name}'".format(name=repo))
+    path = c.fetchall()
+    try:
+        if path[0][0] and path[0][1] == 1:
+            if c.execute("UPDATE repos SET frozen = 0 WHERE name = '{name}'".format(name=repo)):
+                conn.commit()
+                conn.close()
+                print(repo + " unfrozen.")
+        else:
+            sys.exit("Repo already unfrozen.")
+    except TypeError and IndexError:
+        sys.exit("Repo not installed.")
+
 def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("--install", help="Install a repo. Usage: soylent --install username/repo", type=str)
     parser.add_argument("--uninstall", help="Delete a repo. Usage soylent --uninstall username/repo", type=str)
+    parser.add_argument("--update", help="Update all repos", action="store_true")
+    parser.add_argument("--freeze", help="Freeze a repo", type=str)
+    parser.add_argument("--unfreeze", help="Unfreeze a repo", type=str)
     args = parser.parse_args()
     
     if args.install: 
         install(args.install)
     elif args.uninstall:
         uninstall(args.uninstall)
-
+    elif args.update:
+        update()
+    elif args.freeze:
+        freeze(args.freeze)
+    elif args.unfreeze:
+        unfreeze(args.unfreeze)
 parse_arguments()
 
 
